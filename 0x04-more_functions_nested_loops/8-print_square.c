1be0d9a693f67e5323136d1e1ee79277b7e138ec
Omar Gohary <omar.beh27@gmail.com>
2023-07-20 23:51:01 +0000

    #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,11 @@
 */
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
-	int j;
-
-	for (i = 1; i <= size; i++)
+	for (i = 0; i <= size; i++)
 	{
-		for (j = 1; j <= size; j++)
+		for (j = 0; j <= size; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
