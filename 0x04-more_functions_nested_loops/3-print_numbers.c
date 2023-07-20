61c89ab8681037ec74e352d4d048da8ae32e54aa
Omar Gohary <omar.beh27@gmail.com>
2023-07-20 16:35:15 +0000

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 1 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + 48)
+	}
+	_putchar('\n');
+}
