62a53f4d21da9e0122ede968a87204ce7a3e5d3b
Omar Gohary <omar.beh27@gmail.com>
2023-08-04 21:46:14 +0000

    2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: para
+ * @argv: para
+ * Return: 0 (success)
+*/
+
+int main(int argc, char const *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
