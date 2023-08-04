0e9dd4c71512a87ff0ca37363f28948d3f34630f
Omar Gohary <omar.beh27@gmail.com>
2023-08-04 21:39:58 +0000

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: para
+ * @argv[]: para
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
