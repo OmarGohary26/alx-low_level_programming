d7c87939061a0394320f4b7002fd4397ffd68e72
Omar Gohary <omar.beh27@gmail.com>
2023-08-11 00:06:30 +0000

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,12 +22,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < l2)
 		s = malloc(sizeof(char) * (l1 + n + 1));
 	else
-		s = malloc(sizeof(char) * (l1 + len2 + 1));
+		s = malloc(sizeof(char) * (l1 + l2 + 1));
 
 	if (s == NULL)
 		return (NULL);
 
-	while (i < len1)
+	while (i < l1)
 	{
 		s[i] = s1[i];
 		i++;
@@ -43,4 +43,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	return (s);
 }
-
