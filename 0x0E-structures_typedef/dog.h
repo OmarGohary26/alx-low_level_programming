19426d08d796005355e13edaf828d71e69b22da7
Omar Gohary <omar.beh27@gmail.com>
2023-08-14 22:54:49 +0000

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Entry point
+ * @name: vara
+ * @age: vara
+ * @owner: vara
+ * Description: struct to create dog info
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
