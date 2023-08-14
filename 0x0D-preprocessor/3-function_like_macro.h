6c043c4077e4c7dd5e5b23ab26529f7f88f7c598
Omar Gohary <omar.beh27@gmail.com>
2023-08-14 22:41:44 +0000

    Abs

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#define _FUNCTION_LIKE_MACRO_H
 #ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? (-(x)):(x))
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 #endif
