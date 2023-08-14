f3692236b27eea485255d29ec8bc32c56d8d24d2
Omar Gohary <omar.beh27@gmail.com>
2023-08-14 23:17:57 +0000

    Info dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,9 +1,10 @@
 #include "dog.h"
 #include <stdio.h>
+
 /**
  * print_dog - Entry point
  * @d: para
- * Description: Prints the dog info
+ * Return: void
 */
 
 void print_dog(struct dog *d)
@@ -11,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		printf("Age: %f\n", d->name);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
