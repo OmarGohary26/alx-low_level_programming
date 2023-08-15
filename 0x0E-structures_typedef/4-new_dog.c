b16cddf37d8c3ef34258602142ad437779863951
Omar Gohary <omar.beh27@gmail.com>
2023-08-15 00:16:23 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,6 +30,7 @@ char *_strcpy(char *dest, char *src)
 	for (i = 0; src[i]; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
+
 	return (dest);
 }
 
@@ -49,7 +50,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	d = (dog_t *) malloc(sizeof(dog_t));
-	if (dog == NULL)
+	if (d == NULL)
 		return (NULL);
 
 	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
