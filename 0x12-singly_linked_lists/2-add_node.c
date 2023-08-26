a0202d48ec0c35a6bb2ea8f4c0dd08578a64b598
omar gohary <omar.beh27@gail.com>
2023-08-26 01:23:10 +0000

    linked list 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,25 +11,26 @@ list_t *add_node(list_t **head, const char *str)
 {
 	if (!head || !str)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	list_t *n_head = malloc(sizeof(list_t));
 
 	if (!n_head)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	n_head->str = strdup(str);
 	if (!n_head->str)
-		{
-			free(n_head);
-			return NULL;
-		}
+	{
+		free(n_head);
+		return (NULL);
+	}
 
 	n_head->len = _strlen(n_head->str);
 	n_head->next = *head;
 	*head = n_head;
-	return n_head;
+
+	return (n_head);
 }
