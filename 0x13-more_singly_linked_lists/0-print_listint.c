fdabc397c11c4486052462509ae3a52275bf3bfb
omar gohary <omar.beh27@gail.com>
2023-08-29 19:58:35 +0000

    linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the linked list
+ * @h: pointer of the head
+ * Return: list's size
+*/
+
+size_t print_lsitint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
